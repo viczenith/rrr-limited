5a241c61bb24375ba7c36e671c0875d6f9604f2d
Viczenith <viczenithgodwin@gmail.com>
2025-10-21 11:01:00 +0100

    Fix image paths with proper base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/rrr-limited/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
